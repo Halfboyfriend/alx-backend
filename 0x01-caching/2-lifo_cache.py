1974a3329cc866ccaf3ec0f109d3b7bddf86e718
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-07-26 00:19:03 +0100

    delete testing

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -22,7 +22,7 @@ class LIFOCache(BaseCaching):
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
             last = self.queue.pop()
             del self.cache_data[last]
-            print(f'DISCARD: {last}\n')
+            print(f'\nDISCARD: {last}')
 
         self.cache_data[key] = item
         self.queue.append(key)
@@ -31,3 +31,17 @@ class LIFOCache(BaseCaching):
         if key is None:
             return None
         return self.cache_data[key, None]
+my_cache = LIFOCache()
+my_cache.put("A", "Hello")
+my_cache.put("B", "World")
+my_cache.put("C", "Holberton")
+my_cache.put("D", "School")
+my_cache.print_cache()
+my_cache.put("E", "Battery")
+my_cache.print_cache()
+my_cache.put("C", "Street")
+my_cache.print_cache()
+my_cache.put("F", "Mission")
+my_cache.print_cache()
+my_cache.put("G", "San Francisco")
+my_cache.print_cache()
