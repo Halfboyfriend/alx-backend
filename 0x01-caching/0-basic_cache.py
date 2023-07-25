7b20f7e0860e3b2d4bdbc0c6e254ce5d2c59104b
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-07-25 23:10:40 +0100

    cache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -5,11 +5,25 @@ from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
+    """
+    BasicCaching
+    """
     def put(self, key, item):
+        """
+
+        :param key:
+        :param item:
+        :return:
+        """
         if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
+        """
+        
+        :param key:
+        :return:
+        """
         if key is None:
             return None
         return self.cache_data.get(key, None)
