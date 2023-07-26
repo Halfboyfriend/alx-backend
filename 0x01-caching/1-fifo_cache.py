91bc9a5333ccd78fa00534bf42fbe4c1f4b56dd3
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-07-26 17:34:48 +0100

    cache

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -10,6 +10,8 @@ class FIFOCache(BaseCaching):
     """
 
     def __init__(self):
+        """
+        Initializing"""
         super().__init__()
         self.queue = []
 
@@ -23,15 +25,18 @@ class FIFOCache(BaseCaching):
         if key is None or item is None:
             pass
 
-        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            oldest_key = self.queue.pop(0)
-            del self.cache_data[oldest_key]
-            print(f"DISCARD: {oldest_key}\n")
+        else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                oldest_key = self.queue.pop(0)
+                del self.cache_data[oldest_key]
+                print(f"DISCARD: {oldest_key}\n")
 
-        self.cache_data[key] = item
-        self.queue.append(key)
+            self.cache_data[key] = item
+            self.queue.append(key)
 
     def get(self, key):
-        if key is None:
-            return None
-        return self.cache_data.get(key, None)
+        """
+        Return None"""
+        if key is not None and key in self.cache_data.keys():
+            return self.cache_data[key]
+        return None
