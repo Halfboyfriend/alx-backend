fa107e02acd5c188678298886e75780139cbf5ba
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-07-26 17:32:10 +0100

    cache

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """ BaseCaching module
 """
 from base_caching import BaseCaching
@@ -6,35 +6,32 @@ from base_caching import BaseCaching
 
 class FIFOCache(BaseCaching):
     """
-    FIFOCache defines a FIFO caching system
+    FIFOCACHE
     """
 
     def __init__(self):
-        """
-        Initialize the class with the parent's init method
-        """
         super().__init__()
-        self.order = []
+        self.queue = []
 
     def put(self, key, item):
         """
-        Cache a key-value pair
+
+        :param key:
+        :param item:
+        :return:
         """
         if key is None or item is None:
             pass
-        else:
-            length = len(self.cache_data)
-            if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
-                print("DISCARD: {}".format(self.order[0]))
-                del self.cache_data[self.order[0]]
-                del self.order[0]
-            self.order.append(key)
-            self.cache_data[key] = item
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            oldest_key = self.queue.pop(0)
+            del self.cache_data[oldest_key]
+            print(f"DISCARD: {oldest_key}\n")
+
+        self.cache_data[key] = item
+        self.queue.append(key)
 
     def get(self, key):
-        """
-        Return the value linked to a given key, or None
-        """
-        if key is not None and key in self.cache_data.keys():
-            return self.cache_data[key]
-        return None
+        if key is None:
+            return None
+        return self.cache_data.get(key, None)
