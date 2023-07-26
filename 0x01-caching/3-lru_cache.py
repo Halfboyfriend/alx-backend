a390e37014f6c0c7e0ab648bac6813cbb5210c90
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-07-26 17:49:05 +0100

    cache3

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" BaseCaching module
+"""
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """
+    FIFOCache defines a FIFO caching system
+    """
+    def __init__(self):
+        """
+        Initialize the class with the parent's init method
+        """
+        super().__init__()
+        self.stack = []
+
+    def put(self, key, item):
+        """
+        Cache a key-value pair
+        """
+        if key is None or item is None:
+            pass
+        else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
+                print("DISCARD: {}".format(self.stack[0]))
+                del self.cache_data[self.stack[0]]
+                del self.stack[0]
+            if key in self.stack:
+                del self.stack[self.stack.index(key)]
+            self.stack.append(key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Must return the value in self.cache_data linked to key.
+        """
+        if key is not None and key in self.cache_data.keys():
+            del self.stack[self.stack.index(key)]
+            self.stack.append(key)
+            return self.cache_data[key]
+        return None
