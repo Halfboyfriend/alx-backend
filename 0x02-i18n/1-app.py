03ee547044b5d29b1ac7612db38feeeb65b6e687
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-08-01 15:39:39 +0100

    init-flask

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 app = Flask(__name__)
@@ -8,15 +8,18 @@ class Config:
     LANGUAGES = ['en', 'fr']
 
 babel = Babel(app)
-app.config(Config)
-babel.default_locale = 'en'
-babel.default_timezone = 'UTC'
+app.config.from_object(Config)
 
+
+@babel.localeselector
+def get_locale ():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
 def home():
-    return render_template('0-index.html')
+    locale = get_locale()
+    return render_template('2-index.html', loc=locale)
 
 
 if __name__ == '__main__':
