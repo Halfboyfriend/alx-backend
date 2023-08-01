b4d20a582f503b5aa2531784287643fda1a01bd2
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-08-01 04:35:28 +0100

    uni-Testing

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -11,21 +11,26 @@ from utils import (access_nested_map, get_json, memoize)
 
 
 class TestAccessNestedMap(unittest.TestCase):
-
+    """Tests for access_nested_map Functions"""
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
-    def test_access_nested_map(self, nested_map, path, expected_result):
+    def test_access_nested_map(self, nested_map: Dict, 
+                            path: Tuple[str], 
+                            expected_result: Union[Dict, int]) -> None:
+        """Test access_nested_map"""
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
 
     @parameterized.expand([
-        ({"a": 1}, ("b",)), 
-        ({"a": {"b": 2}}, ("a", "c")), 
-        ({}, ("a",)),  
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError),
     ])
 
-    def test_access_nested_map_exception(self, nested_map, path):
-        with self.assertRaises(KeyError):
+    def test_access_nested_map_exception(self, 
+            nested_map: Dict, path, 
+            exception: Exception) -> None:
+        """Testing for exceptions"""
+        with self.assertRaises(exception):
             access_nested_map(nested_map, path)
