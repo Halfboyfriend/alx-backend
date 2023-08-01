0680fd4a51d543cb4ee1ccbbeb538bcd6cb5749b
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-08-01 15:30:50 +0100

    init-flask

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,9 +1,17 @@
 from flask import Flask, render_template
-
+from flask_babel import Babel
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+class Config:
+    LANGUAGES = ['en', 'fr']
+
+babel = Babel(app)
+app.config(Config)
+babel.default_locale = 'en'
+babel.default_timezone = 'UTC'
+
 
 
 @app.route('/')
