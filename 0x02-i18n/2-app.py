350bac421761711cbdf59727aaf9e02b4980e172
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-08-01 15:43:21 +0100

    init-flask

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -15,10 +15,15 @@ babel = Babel(app)
 app.config.from_object(Config)
 
 
+@babel.localeselector
+def get_locale ():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 @app.route('/')
 def home():
-    return render_template('1-index.html')
+    locale = get_locale()
+    return render_template('2-index.html', loc=locale)
 
 
 if __name__ == '__main__':
